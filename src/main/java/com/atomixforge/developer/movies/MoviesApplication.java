65ed505db0df7da93f558173b74ff4fcb884f8bb
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-05 12:50:30 -0500

    first commit

---
+++ src/main/java/com/atomixforge/developer/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package com.atomixforge.developer.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
